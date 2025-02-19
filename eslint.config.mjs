163eaea8fd68e328943168894db6e28bcd199fa8
osama <tn7h.btrkez@gmail.com>
2025-02-19 15:19:01 -0800

    Fixed TypeScript any issue in botId route

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // تعطيل التحذير عن استخدام any
+    },
+  },
 ];
 
 export default eslintConfig;
