5891813dadeb4bda09115005569fe86088d19e61
osama <tn7h.btrkez@gmail.com>
2025-02-22 17:13:38 +0000

    Feb 22, 2025, 8:13 PM

--- src/app/components/BottomNavBar.tsx
+++ src/app/components/BottomNavBar.tsx
@@ -1,18 +1,25 @@
-// components/BottomNavBar.tsx
+// src/components/BottomNavBar.tsx
 import React from "react";
 import { FaHome, FaHeart, FaPlus, FaComment, FaUser } from "react-icons/fa";
-import "./BottomNavBar.css"; // استيراد ملف CSS إذا لزم الأمر
 
 export default function BottomNavBar() {
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-[#4B0082] p-4 flex justify-around items-center">
-      <button><FaHome className="text-white text-2xl" /></button>
-      <button><FaHeart className="text-white text-2xl" /></button>
+      <button className="text-white text-2xl">
+        <FaHome />
+      </button>
+      <button className="text-white text-2xl">
+        <FaHeart />
+      </button>
       <div className="bg-purple-500 p-3 rounded-full">
         <FaPlus className="text-white text-2xl" />
       </div>
-      <button><FaComment className="text-white text-2xl" /></button>
-      <button><FaUser className="text-white text-2xl" /></button>
+      <button className="text-white text-2xl">
+        <FaComment />
+      </button>
+      <button className="text-white text-2xl">
+        <FaUser />
+      </button>
     </nav>
   );
 }
