f2dd6d2fe2178254b29dbeb6726abd7dc013aaf8
Osama Alharthi <tnn7hh@gmail.com>
2025-05-19 23:43:35 +0000

    May 19, 2025, 4:43 PM

--- prisma/migrations/20250218161014_last/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Bot" DROP CONSTRAINT "Bot_creatorId_fkey";
-
--- AlterTable
-ALTER TABLE "Bot" ALTER COLUMN "creatorId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Bot" ADD CONSTRAINT "Bot_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20250219121536_add_api_key_field/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Bot" ADD COLUMN     "accent" TEXT,
-ADD COLUMN     "apiKey" TEXT;
